943f1d0d6a3e06f0c55b7b4d5e40e6c15dbcfa17
Spencer Hurrle <spencer.hurrle@gmail.com>
2021-06-10 13:50:26 -0400

    Added first move assignment

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -14,12 +14,19 @@ int main(void){
 		std::cout << "\nPlayer Two, enter your name.\n";
 		std::cin >> player2;
 		
-		std::cout << "'X' goes first. 'X' player will be randomly decided.\n";
+		std::cout << "The first move will be decided by the computer.\n";
 		int first_move = rand() % 2 + 1;
-		std::cout << first_move << "\n";
+		std::cout << first_move << "\n"; //Generates random number to determine first player
+		
+		if (first_move == 1){
+			std::cout << player1 << " goes first!\n";
+		} else{
+			std::cout << player2 << " goes first!\n";
+		}
 		
 		if (player1 == "x"){
 			playing = false;
 		}
 	}
 }
+
