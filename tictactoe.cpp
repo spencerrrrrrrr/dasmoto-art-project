35fe9a0db391cee2e28e9041e3a1419431f59e35
Spencer Hurrle <spencer.hurrle@gmail.com>
2021-06-23 13:44:06 -0400

    Game is operational, but not playable yet.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,13 +2,6 @@
 #include <stdlib.h>
 //#include tictactoe.hpp
 
-/*
-TODO List
--Take player selections - line 42
-Check that selections have not already been claimed by either player
-Fix tile references upon player selections
-*/
-
 int main(void){
 	bool playing = true;
 	std::string player1 = "";
@@ -16,7 +9,7 @@ int main(void){
 	std::string playerX = "";
 	std::string playerO = "";
 	int turn = 1;
-	std::string selection = "";
+	char selection = ' ';
 	char tiles[] = {'1','2','3', '4', '5', '6', '7', '8', '9'};
 	
 	while (playing) {
@@ -28,7 +21,7 @@ int main(void){
 		
 		std::cout << "The first move will be decided by the computer.\n";
 		int first_move = rand() % 2 + 1;
-		std::cout << first_move << "\n"; //Generates random number to determine first player
+		//Generates random number to determine first player
 		
 		if (first_move == 1){
 			std::cout << player1 << " goes first!\n";
@@ -38,49 +31,60 @@ int main(void){
 			std::cout << player2 << " goes first!\n";
 			playerX = player2;
 			playerO = player1;
+		}		
+		if (turn == 1){
+			std::cout << playerX << ", choose a tile to place an X in by selecting a number 1-9.\n";
+			std::cout << "You may not take a tile that has already been claimed by a player.\n";
+			std::cin >> selection;
+			//Separate function for selection validation. Put into functions file later.
+			for (int x = 0; x < 9; x++){
+				if (selection == tiles[x] && selection != 'X' && selection != 'O' && turn == 1){
+					tiles[x] = 'X';
+				} else if (selection == tiles[x] && selection != 'X' && selection != 'O' && turn == 'O'){
+					tiles[x] = 'O'; 
+				} else{
+				std::cout << "That is an invalid choice. Please select a valid tile.\n";
+				}
+			}
+			turn++;
+		} else{
+			std::cout << playerO << ", choose a tile to place an X in by selecting a number 1-9.\n";
+			std::cout << "You may not take a tile that has already been claimed by a player.\n";
+			std::cin >> selection;
+			for (int x = 0; x < 9; x++){
+				if (selection == tiles[x] && selection != 'X' && selection != 'O' && turn == 1){
+					tiles[x] = 'X';
+				} else if (selection == tiles[x] && selection != 'X' && selection != 'O' && turn == 'O'){
+					tiles[x] = 'O'; 
+				} else{
+				std::cout << "That is an invalid choice. Please select a valid tile.\n";
+				}
+			}
+			turn--;
 		}
-				
-		for (int i = 0 ; i < 9; i++){
-			if (turn == 1){
-				std::cout << playerX << ", choose a tile to place an X in by selecting a number 1-9.\n";
-				std::cout << "You may not take a tile that has already been claimed by a player.\n";
-				std::cin >> selection;
-				//Separate function for selection validation. Put into functions file later.
-				if (tiles{selection} != 'X' && tiles{selection} != 'O' && turn == 1){
-					std::replace(tiles[0], tiles[8], selection, 'X');
-				} else if (tiles{selection} != 'X') && (tiles{selection] != 'O') && (turn == 2){
-					std::replace(tiles[0], tiles[8], selection, 'O');
-				} else{
-					std::cout << "That is an invalid choice. Please select a valid tile.\n";
-				}
-				turn++;
+		//Win Check. Fill out win conditions
+		if (tiles[0] == tiles[1] && tiles[0] == tiles[2] ||
+		tiles[3] == tiles[4] && tiles[3] == tiles[5] ||
+		tiles[6] == tiles[7] && tiles[6] == tiles[8] ||
+		tiles[6] == tiles[3] && tiles[6] == tiles[0] ||
+		tiles[7] == tiles[4] && tiles[7] == tiles[1] ||
+		tiles[8] == tiles[5] && tiles[8] == tiles[2] ||
+		tiles[6] == tiles[4] && tiles[6] == tiles[2] ||
+		tiles[0] == tiles[4] && tiles[0] == tiles[8]){
+			if (turn == 2){
+				std::cout << playerX << " wins!";
+				playing = false;
 			} else{
-				std::cout << playerO << ", choose a tile to place an X in by selecting a number 1-9.\n";
-				std::cout << "You may not take a tile that has already been claimed by a player.\n";
-				std::cin >> selection;
-				turn--;
+				std::cout << playerO << " wins!";
+				playing = false;
 			}
-			//Win Check. Fill out win conditions
-			if (tiles[0] == tiles[1] && tiles[0] == tiles[2]) ||
-			(tiles[3] == tiles[4] && tiles[3] == tiles[5]) ||
-			(tiles[6] == tiles[7] && tiles[6] == tiles[8]) ||
-			(tiles[6] == tiles[3] && tiles[6] == tiles[0]) ||
-			(tiles[7] == tiles[4] && tiles[7] == tiles[1]) ||
-			(tiles[8] == tiles[5] && tiles[8] == tiles[2]) ||
-			(tiles[6] == tiles[4] && tiles[6] == tiles[2]) ||
-			(tiles[0] == tiles[4] && tiles[0] == tiles[8]){
-				if (turn == 2){
-					std::cout << playerX << " wins!";
-				} else{
-					std::cout << playerO << " wins!";
-				}
-				
-			}
+			
 		}
 		
-		if (player1 == "x"){
+		if (player1 == "x" || selection == 'q'){
 			playing = false;
 		}
 	}
 }
+
 
