8b699500e6670430c7ace920fe21ae9efe43a678
Spencer Hurrle <spencer.hurrle@gmail.com>
2021-07-07 07:45:01 -0400

    update

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <stdlib.h>
-//#include tictactoe.hpp
-//flip if statements to catch bad input
+#include <stdlib.h> //for rand num
+#include "tictactoe.hpp"
 
 int main(void){
 	bool playing = true;
@@ -10,8 +9,8 @@ int main(void){
 	std::string playerX = "";
 	std::string playerO = "";
 	int turn = 1;
-	char selection = ' ';
-	char tiles[] = {'1','2','3', '4', '5', '6', '7', '8', '9'};
+	
+	char tiles[9] = {'1','2','3', '4', '5', '6', '7', '8', '9'};
 	
 	std::cout << "Let's play TicTacToe!\n";
 	std::cout << "Player One, enter your name.\n";
@@ -34,51 +33,12 @@ int main(void){
 	}		
 	
 	while (playing) {
-
 		if (turn == 1){
-			std::cout << playerX << ", choose a tile to place an X in by selecting a number 1-9.\n";
-			std::cout << "You may not take a tile that has already been claimed by a player.\n";
-			std::cin >> selection;
-			//Separate function for selection validation. Put into functions file later.
-			
-			if (selection != 'X' && selection != 'O' && turn == 1){
-				for (int x = 0; x < 9; x++){
-					if (selection == tiles[x]){
-						tiles[x] = 'X';
-					}
-				}
-			} else if (selection != 'X' && selection != 'O' && turn == 2){
-				for (int x = 0; x < 9; x++){
-					if (selection == tiles[x]){
-						tiles[x] = 'O';
-					}
-				}
-			} else{
-				std::cout << "That is an invalid choice. Please select a valid tile.\n";
-			}
-			turn++;
+			switch_playr_move(playerX, tiles);
 		} else{
-			std::cout << playerO << ", choose a tile to place an X in by selecting a number 1-9.\n";
-			std::cout << "You may not take a tile that has already been claimed by a player.\n";
-			std::cin >> selection;
-			if (selection != 'X' && selection != 'O' && turn == 1){
-				for (int x = 0; x < 9; x++){
-					if (selection == tiles[x]){
-						tiles[x] = 'X';
-					}
-				}
-			} else if (selection != 'X' && selection != 'O' && turn == 2){
-				for (int x = 0; x < 9; x++){
-					if (selection == tiles[x]){
-						tiles[x] = 'O';
-					}
-				}
-			} else{
-				std::cout << "That is an invalid choice. Please select a valid tile.\n";
-			}
-			turn--;
+			switch_playr_move(playerO, tiles);
 		}
-		//Win Check. Fill out win conditions
+		
 		if (tiles[0] == tiles[1] && tiles[0] == tiles[2] ||
 		tiles[3] == tiles[4] && tiles[3] == tiles[5] ||
 		tiles[6] == tiles[7] && tiles[6] == tiles[8] ||
@@ -97,7 +57,7 @@ int main(void){
 			
 		}
 		
-		if (player1 == "x" || selection == 'q'){
+		if (player1 == "q" || player2 == "q" || selection == 'q'){
 			playing = false;
 		}
 	}
--- tictactoe_functions.cpp
+++ tictactoe_functions.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 void big_game_board() {
 	std::cout << "  77777 #  888   #  999   \n" //All tiles are 5 spaces vertically and 7 spaces horizontally	
 	std::cout << "     7  # 8   8  # 9   9  \n" //Use list to offer tile selections 1 - 9
@@ -19,9 +21,38 @@ void big_game_board() {
 }
 
 void game_board(){
-	std::cout << "7|8|9\n";
+	std::cout << tiles[6] << "|" << tiles[7] << "|" << tiles[8];
 	std::cout << "-----\n";
-	std::cout << "4|5|6\n";
+	std::cout << tiles[3] << "|" << tiles[4] << "|" << tiles[5];
 	std::cout << "-----\n";
-	std::cout << "1|2|3\n";
+	std::cout << tiles[0] << "|" << tiles[1] << "|" << tiles[2];
+}
+
+void switch_playr_move(std::string actv_playr, char tiles[9]){
+	std::string actv_playr = "";
+	char selection = ' ';
+	bool valid = true;
+	
+	std::cout << actv_playr << ", choose a tile to place an X in by selecting a number 1-9.\n";
+	std::cout << "You may not take a tile that has already been claimed by a player.\n";
+	std::cin >> selection;
+	
+	for (int x = 0; x < 9; x++){
+		if (selection == tiles[x] && turn == 1){
+			tiles[x] = 'X';
+			turn++;
+			valid = true;
+			break;
+		} else if (selection == tiles[x] && turn == 2){
+			tiles[x] = 'O';
+			turn--;
+			valid = true;
+			break;
+		} else {
+			valid = false;
+		}
+	}
+	if (valid == false){
+		std::cout << "That is an invalid choice. Please select a valid tile.\n";
+	}
 }
